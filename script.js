58dcd24df4e59a615e9c9ac5bc261de7b74ea57f
Camila Palomeque <113646128+CamilaPalomeque@users.noreply.github.com>
2022-10-16 20:41:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function iniciarMap(){
+    var coord = {lat:-37.9900064 ,lng: -57.575573};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 15,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+
+function pruebaemail (valor){
+	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+	if(!re.exec(valor)){
+		alert('email no válido');
+	}
+	else alert('email válido');
+}
